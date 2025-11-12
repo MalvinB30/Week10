46a8c382f46c1827ad1b2b43e880dc2283d2bd8b
Malv jahanto <MalvinB30>
2025-11-12 11:22:11 +0700

    Upcasting

---
+++ src/upcasting/Main.java
@@ -0,0 +1,23 @@
+package upcasting;
+
+public class Main {
+    public static void main(String[] args) {
+        Pekerja pekerja = new Pekerja();
+        pekerja.tanyaIdentitas();
+
+        // contoh upcasting
+        pekerja = new CEO();
+        pekerja.tanyaIdentitas();
+
+        // contoh upcasting
+        Karyawan karyawan = new Karyawan();
+        pekerja = (Pekerja) karyawan;
+        pekerja.tanyaIdentitas();
+    }
+}
+
+
+
+
+
+
